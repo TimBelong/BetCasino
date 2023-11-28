0cc2424923e19bd8f093b9439dd42051cb19d412
Tim Belong <mrbelongtim@gmail.com>
2023-11-28 13:44:47 +0100

    added demo for game slider, added games section

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
 let headerSlider = new Swiper('.main__slider', {
-    slidesPerView: 1,
-            spaceBetween: 0,
-            slidesPerGroup: 1,
-            centerSlidesBounds: true,
-            centerInsufficientSlides: true,
-            centerSlides: true,
-            loop: true,
-            loopFillGroupWithBlank: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  centerSlidesBounds: true,
+  centerInsufficientSlides: true,
+  centerSlides: true,
+  loop: true,
+  loopFillGroupWithBlank: true,
 
-   autoplay: {
-      delay: 5000,
-    },
+  autoplay: {
+    delay: 5000,
+  },
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    }
-  });
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
 
-  let gameSlider = new Swiper('.game__slider', {
-    slidesPerView: 5,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-            centerSlidesBounds: true,
-            centerInsufficientSlides: true,
-            centerSlides: true,
-            loop: true,
-            loopFillGroupWithBlank: true,
-    navigation: {
+let gameSlider = new Swiper('.game__slider', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  centerSlidesBounds: true,
+  centerInsufficientSlides: true,
+  centerSlides: true,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-   autoplay: {
+  },
+  autoplay: {
       delay: 5000,
-    },
+  },
+});
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+let gameListItems = document.querySelectorAll('.swiper-slide');
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
-  });
+gameListItems.forEach(item =>{
+    item.addEventListener('mouseenter', function () {
+        this.querySelector('.demo_wrapper').classList.add('_active');
+    });
+
+    item.addEventListener('mouseleave', function () {
+        this.querySelector('.demo_wrapper').classList.remove('_active');
+    });
+});
